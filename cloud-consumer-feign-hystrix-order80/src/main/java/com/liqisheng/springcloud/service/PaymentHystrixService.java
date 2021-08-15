4285d12ea03a47cf131a631d084ccca553502dc3
liqish <liqish@qq.com>
2021-08-15 16:17:02 +0800

    在笔记本电脑上第二次提交，修改Hystrix降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/liqisheng/springcloud/service/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/liqisheng/springcloud/service/PaymentHystrixService.java
@@ -2,7 +2,6 @@ package com.liqisheng.springcloud.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
